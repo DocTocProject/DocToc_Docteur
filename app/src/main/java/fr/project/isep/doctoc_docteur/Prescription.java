a6fbaba4acb3a2ba25636172acfd0c7c6bf4b44f
evamartinez <eva.martinez.2212@gmail.com>
2017-01-31 11:23:23 +0100

    Eva
    It is working well, the map & prescription page are working well but
    not link to any page because need to be done with the server

---
+++ app/src/main/java/fr/project/isep/doctoc_docteur/Prescription.java
@@ -0,0 +1,22 @@
+package fr.project.isep.doctoc_docteur;
+
+import android.os.Bundle;
+import android.support.annotation.IdRes;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+
+/**
+ * Created by martinezeva on 30/01/2017.
+ */
+
+public class Prescription extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prescription);
+
+}}
+
--- app/src/main/java/fr/project/isep/doctoc_docteur/Visitess.java
+++ app/src/main/java/fr/project/isep/doctoc_docteur/Visitess.java
@@ -1,11 +1,17 @@
 package fr.project.isep.doctoc_docteur;
 
 import android.content.Intent;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Visitess extends AppCompatActivity {
+
+    Button button;
+    Button button2;
+    Button button3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,22 @@ public class Visitess extends AppCompatActivity {
         setContentView(R.layout.visites);
     }
     public void OnClickWork (View v) {
-        startActivity(new Intent(this, MapsActivity.class));
+        startActivity(new Intent(this, Prescription.class));
+    }
+
+    public void OnClickView(View v) {
+        if (v.getId() == R.id.button) {
+            Intent intent = new Intent(Visitess.this, Visitesdetails.class);
+            startActivity(intent);
+        }
+        else if(v.getId() == R.id.button2){
+            Intent intent = new Intent(Visitess.this, Visitesdetails.class);
+            startActivity(intent);
+        }
+        else if(v.getId() == R.id.button3){
+            Intent intent = new Intent(Visitess.this, Visitesdetails.class);
+            startActivity(intent);
+
+        }
     }
 }
